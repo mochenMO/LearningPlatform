8ccc5b233755747dc7aff1bb8f2c700ac545a355
麟 逆 <2367267161@qq.com>
2024-04-07 15:10:49 +0800

    new

---
+++ LearningPlatform/server/src/log/log.cpp
@@ -0,0 +1,527 @@
+#include "log.h"
+
+using namespace mochen::log;
+
+// =============================================================================================================
+// 全局区
+
+// 创建全局的 defauleLogEventManager (注意：定义全局函数时也要加上命名空间)
+LogEventManager* mochen::log::getDefaultLogEventManager() // 注意在C++11及以上版本标准中，规定静态局部变量初始化的线程安全性
+{
+	static LogEventManager defauleLogEventManager{};
+	return &defauleLogEventManager;
+}
+
+// 全局函数
+Logger* mochen::log::getDefaultLogger()
+{
+	static Logger defauleLogger("defauleLogger", LogLevel::debug, std::make_shared<ConsoleLogAppender>());
+	return &defauleLogger;
+}
+
+
+// 声明全局的 logLevelString
+const char* logLevelString[5] = { "debug","info","warn","error","fatal" };
+
+
+// =============================================================================================================
+// 格式化函数
+
+
+// [年-月-日 时:分:秒][日志器名称][日志等级][文件名]:[行号][日志信息]
+void mochen::log::logFormatFuntion_detailed(std::stringstream& _ss, LogEvent& _logEvent)
+{
+	char timeString[24] = { 0 };
+	struct tm* pt = nullptr;     // 注意 pt 指向的内存空间由系统管理，无需手动释放
+
+	// 处理时间
+	pt = localtime(&_logEvent.m_timestamp);
+	// [0000-00-00 00:00:00]
+	sprintf(timeString, "%d-%0.2d-%0.2d %0.2d:%0.2d:%0.2d",
+		pt->tm_year + 1900,
+		pt->tm_mon + 1,
+		pt->tm_mday,
+		pt->tm_hour,
+		pt->tm_min,
+		pt->tm_sec);
+
+	// [年-月-日 时:分:秒][日志器名称][日志等级][文件名]:[行号][日志信息]
+	_ss << "[" << timeString << "]";
+	_ss << "[" << *(_logEvent.m_loggername) << "]";
+	_ss << "[" << logLevelString[(int)_logEvent.m_LogLevel] << "]";
+	_ss << "[" << _logEvent.m_filename << "]:";
+	_ss << "[" << _logEvent.m_line << "]";
+	_ss << "[" << _logEvent.m_content << "]";
+	_ss << "\n";
+}
+
+
+// [时:分:秒][日志等级][简要的文件名]:[行号][日志信息]
+void mochen::log::logFormatFuntion_briefly(std::stringstream& _ss, LogEvent& _logEvent)
+{
+	char timeString[24] = { 0 };
+	struct tm* pt = nullptr;     // 注意 pt 指向的内存空间由系统管理，无需手动释放
+
+	// 提取文件名
+	const char* temp = _logEvent.m_filename + strlen(_logEvent.m_filename);   // 把 temp 移到 _logEvent.m_filename 的结尾
+	while (temp != _logEvent.m_filename) {
+		if (*temp == '\\') {
+			++temp;
+			break;
+		} 
+		--temp;
+	}
+
+	// 处理时间
+	pt = localtime(&_logEvent.m_timestamp);
+	// [00:00:00]
+	sprintf(timeString, "%0.2d:%0.2d:%0.2d",
+		pt->tm_hour,
+		pt->tm_min,
+		pt->tm_sec);
+
+	// [时:分:秒][日志等级][简要的文件名]:[行号][日志信息]
+	_ss << "[" << timeString << "]";
+	_ss << "[" << logLevelString[(int)_logEvent.m_LogLevel] << "]";
+	_ss << "[" << temp << "]:";
+	_ss << "[" << _logEvent.m_line << "]";
+	_ss << "[" << _logEvent.m_content << "]";
+	_ss << "\n";
+}
+
+
+// =============================================================================================================
+// class LogAppender
+
+LogAppender::LogAppender()
+{
+	m_pLogFormatFuntion = &logFormatFuntion_briefly;
+}
+
+PLogFormatFuntion LogAppender::getFormatFuntion()
+{
+	return m_pLogFormatFuntion;
+}
+
+void LogAppender::setFormatFuntion(PLogFormatFuntion _pLogFormatFuntion)
+{
+	m_pLogFormatFuntion = _pLogFormatFuntion;
+}
+
+
+
+// =============================================================================================================
+// class ConsoleLogAppender
+
+ConsoleLogAppender::ConsoleLogAppender()
+{
+	m_pLogFormatFuntion = &logFormatFuntion_briefly;
+}
+
+ConsoleLogAppender::ConsoleLogAppender(PLogFormatFuntion _pLogFormatFuntion)
+{
+	m_pLogFormatFuntion = _pLogFormatFuntion;
+}
+
+void ConsoleLogAppender::log(const char* _massage)
+{
+	printf("%s", _massage);
+}
+
+
+// =============================================================================================================
+// class FileLogAppender
+
+FileLogAppender::FileLogAppender()
+{
+	m_pLogFormatFuntion = &logFormatFuntion_detailed;
+	m_path = "";
+	m_filename = "";
+	m_fp = nullptr;
+	m_maxSize = M_FILEMAXSIZE;
+}
+
+
+FileLogAppender::FileLogAppender(const std::string& _filename, int _maxSize)
+{
+	m_pLogFormatFuntion = &logFormatFuntion_detailed;
+	m_maxSize = _maxSize;
+
+	const char* filename = _filename.c_str() + _filename.size();
+	while (filename != _filename.c_str()) {
+		if (*filename != '\\' && *filename != '/') {
+			--filename;
+		}
+		else {
+			++filename;
+			break;
+		}
+	}
+
+	m_filename = filename;
+	m_path = _filename.substr(0, filename - _filename.c_str());
+	
+	std::string temp = _filename;
+	temp += ".log";
+	if ((m_fp = fopen(temp.c_str(), "a")) == nullptr) {
+		throw std::logic_error("FileLogAppender::FileLogAppender(): open file failded");    // std::logic_error异常对象中会自动包含文件名和行号等调试信息
+	}
+}
+
+
+FileLogAppender::~FileLogAppender()
+{
+	clear();
+}
+
+
+FileLogAppender::FileLogAppender(FileLogAppender&& _value) noexcept
+{
+	m_path = _value.m_path;
+	m_filename = _value.m_filename;
+	m_fp = _value.m_fp;
+	m_maxSize = m_maxSize;
+
+	_value.m_fp = nullptr;
+}
+
+
+void FileLogAppender::operator=(FileLogAppender&& _value) noexcept
+{
+	clear();
+
+	m_path = _value.m_path;
+	m_filename = _value.m_filename;
+	m_fp = _value.m_fp;
+	m_maxSize = m_maxSize;
+
+	_value.m_fp = nullptr;
+}
+
+
+void FileLogAppender::log(const char* _message)
+{
+	if (m_fp == nullptr) {
+		throw std::logic_error("FileLogAppender::log(): null pointer, <m_fp> is nullptr");
+	}
+
+	if (getFileSize() >= m_maxSize) {
+		scrolling();  // 滚动
+	}
+
+	fwrite(_message, strlen(_message), 1, m_fp);
+	fflush(m_fp);
+}
+
+void FileLogAppender::clear()
+{
+	if (m_fp != nullptr) {
+		fclose(m_fp);
+		m_fp = nullptr;
+	}
+	m_filename = "";
+}
+
+void FileLogAppender::open(const std::string& _filename)
+{
+	clear();
+
+	const char* filename = _filename.c_str() + _filename.size();
+	while (filename != _filename.c_str()) {
+		if (*filename != '\\' && *filename != '/') {
+			--filename;
+		}
+		else {
+			++filename;
+			break;
+		}
+	}
+
+	m_filename = filename;
+	m_path = _filename.substr(0, filename - _filename.c_str());
+
+	std::string temp = _filename;
+	temp += ".log";
+
+	if ((m_fp = fopen(temp.c_str(), "a")) == nullptr) {
+		throw std::logic_error("FileLogAppender::open(): open file failded");    // std::logic_error异常对象中会自动包含文件名和行号等调试信息
+	}
+}
+
+
+std::string FileLogAppender::getFilename()
+{
+	return m_filename;
+}
+
+
+int FileLogAppender::getFileSize()
+{
+	int size = 0;
+	fseek(m_fp, 0, SEEK_END);    // 移动文件指针到文件结尾
+	size = ftell(m_fp);          // 获取当前文件指针的位置，即文件大小，单位字节
+	fseek(m_fp, 0, SEEK_SET);    // 移动文件指针到文件开头
+	return size;
+}
+
+void FileLogAppender::setFileSize(int _size)
+{
+	m_maxSize = _size;
+}
+
+void FileLogAppender::scrolling()
+{
+	if (m_fp != nullptr) {   // 由于没有实现setFilename函数，所以 m_fp != nullptr 时 m_filename != ""
+		fclose(m_fp);
+		m_fp = nullptr;
+	}
+	else {
+		throw std::logic_error("FileLogAppender::scrolling(): null pointer, <m_fp> is nullptr");
+	}
+
+	std::string temp = m_path;
+	temp += m_filename;
+	temp += "_log_";
+
+	char timeString[24] = { 0 };
+	time_t timestamp = time(nullptr);
+	struct tm* pt = localtime(&timestamp);    // 注意 pt 指向的内存空间由系统管理，无需手动释放
+	// 0000-00-00#00-00-00
+	sprintf(timeString, "%d-%0.2d-%0.2d#%0.2d-%0.2d-%0.2d",
+		pt->tm_year + 1900,
+		pt->tm_mon + 1,
+		pt->tm_mday,
+		pt->tm_hour,
+		pt->tm_min,
+		pt->tm_sec);
+
+	// m_filename_log_0000-00-00#00-00-00
+	temp += timeString;
+	temp += ".log";
+
+	if ((m_fp = fopen(temp.c_str(), "a")) == nullptr) {
+		throw std::logic_error("FileLogAppender::scrolling(): open file failded");    // std::logic_error异常对象中会自动包含文件名和行号等调试信息
+	}
+}
+
+
+
+// =============================================================================================================
+// class LogEventManager
+
+LogEventManager::LogEventManager() :
+	m_thread(&LogEventManager::dealLogEvent_threadFuntion, this),  // 注意使用成员函数指针时要加作用域
+	m_mutex(),
+	m_isCanExit(false)
+{
+	m_logEventQueue = (LogEventQueue*)malloc(sizeof(LogEventQueue));
+	m_logEventQueue->m_next = nullptr;
+
+	m_ptrWrite = m_logEventQueue;
+	m_ptrRead = m_logEventQueue;
+
+	m_LogAppenderListMap = new std::map<std::string, std::list<std::shared_ptr<LogAppender>>>{};
+}
+
+
+LogEventManager::~LogEventManager()
+{
+	m_isCanExit = true;         // 注意 m_isCanExit = true 且处理完日志队列中所有数据后才能退出
+	m_thread.join();            // 等待处理完日志队列中所有数据
+	clearLogEventQueue();
+	clearLogAppenderListMap();
+
+	printf("~LogEventManager\n");  // LogEventManager应该是最后一个析构的
+	_CrtDumpMemoryLeaks();         // 检测内存是否泄露
+}
+
+
+
+void LogEventManager::clearLogEventNodeData(LogEventQueue* _node)
+{
+	delete _node->m_data.m_loggername;
+	free(_node->m_data.m_content);        // 释放申请的资源
+	free(_node);
+}
+
+void LogEventManager::clearLogEventQueue()
+{
+	LogEventQueue* tempNode = m_logEventQueue;
+	m_logEventQueue = m_logEventQueue->m_next;
+	free(tempNode);      // 先把哨兵节点销毁掉，因为哨兵节点中没有存数据，所以不能 free(m_data.m_content)
+
+	while (m_logEventQueue != nullptr) {
+		tempNode = m_logEventQueue;
+		m_logEventQueue = m_logEventQueue->m_next;
+		clearLogEventNodeData(tempNode);
+	}
+	m_logEventQueue = nullptr;
+}
+
+
+void LogEventManager::clearLogAppenderListMap()
+{
+	for (auto mapIt = m_LogAppenderListMap->begin(); mapIt != m_LogAppenderListMap->end(); ++mapIt) {
+		// list = mapIt->second
+		for (auto vecterIt = mapIt->second.begin(); vecterIt != mapIt->second.end(); ++vecterIt) {
+			vecterIt->reset();   // 释放智能指针
+		}
+	}
+	delete m_LogAppenderListMap;
+}
+
+
+void LogEventManager::addAppender(std::string _loggername, std::shared_ptr<LogAppender> _appender)
+{
+	m_mutex.lock();  // 注意：千万别忘记加锁因为树结构有个元素之间有关联性它不是独立的 ！！！！
+	(*m_LogAppenderListMap)[_loggername].push_back(_appender);   // map是个复杂的黑盒类，无法用原子操作
+	m_mutex.unlock();
+}
+
+
+bool LogEventManager::isFindLogger(const std::string& _loggername)
+{
+	return (m_LogAppenderListMap->find(_loggername) != m_LogAppenderListMap->end());
+}
+
+
+std::map<std::string, std::list<std::shared_ptr<LogAppender>>>* LogEventManager::getLogAppenderListMap()
+{
+	return m_LogAppenderListMap;
+}
+
+void LogEventManager::dealLogEvent_threadFuntion()
+{
+	LogEventQueue* tempNode = nullptr;    // 临时保存可读的节点，读取完后，辅助删除
+	std::list<std::shared_ptr<LogAppender>>* tempList = nullptr;  // 用指针类型避免调用拷贝或者移动函数
+	LogEvent tempData = { 0 };
+	std::stringstream ss;
+
+	 
+	while (m_isCanExit == false || m_ptrRead->m_next != nullptr)  // 当 m_ptrRead = m_ptrWrite 时 m_ptrRead->m_next 也会受到内存屏障保护
+	{
+		if (m_ptrRead->m_next != nullptr)
+		{
+			tempNode = m_ptrRead->m_next;
+			m_ptrRead = m_ptrRead->m_next;
+
+			tempData = tempNode->m_data;
+			tempList = &(*m_LogAppenderListMap)[*(tempData.m_loggername)];
+
+			// 遍历 tempList 中所有的 LogAppender
+			for (auto it = tempList->begin(); it != tempList->end(); ++it) {  // 或者用 (*(*it)).getType();
+				it->get()->getFormatFuntion()(ss, tempData);
+				it->get()->log(ss.str().c_str());
+				ss.str("");   // 清空 stringstream
+			}
+
+			if (m_logEventQueue-> m_next != m_ptrRead) {   // 头删法。当 m_logEventQueue = m_ptrWrite 时 m_logEventQueue->m_next 也会受到内存屏障保护（注意该删除方式最后 m_ptrRead=m_ptrWrite 只存在一个哨兵节点和一个元素节点共两个节点）
+				tempNode = m_logEventQueue->m_next;
+				m_logEventQueue->m_next = tempNode->m_next;
+				clearLogEventNodeData(tempNode);
+			}
+
+		}
+
+	}
+
+}
+
+
+void LogEventManager::addLogEvent(LogEvent _logEvent)
+{
+	LogEventQueue* newNode = (LogEventQueue*)malloc(sizeof(LogEventQueue));
+	newNode->m_next = nullptr;
+	newNode->m_data = _logEvent;
+
+	while (true) // 原子操作通常要配合自旋锁
+	{  
+		LogEventQueue* currentWrite = m_ptrWrite;     // m_ptrWrite是原子操作的对象，因此可以直接读取用于判断，但不能 m_ptrWrite = XXXX 或 m_ptrWrite->next = XXXX
+		LogEventQueue* next = currentWrite->m_next;   // 注意：不是 m_ptrWrite->m_next
+
+		if (currentWrite == m_ptrWrite) {
+			if(next == nullptr) {
+				if (InterlockedCompareExchangePointer((PVOID*)(&(currentWrite->m_next)), newNode, nullptr) == nullptr) {   // 注意用 currentWrite->m_next 而不是 m_ptrWrite->next 是因为 m_ptrWrite 可能会变。同时当 currentWrite == m_ptrWrite 时，m_ptrWrite->next 也会受到内存屏障保护
+					InterlockedExchangePointer((PVOID*)(&(m_ptrWrite)), newNode); // 注意用 newNode 赋值，注意最后即使 m_ptrWrite 不在最尾端也没关系
+					break;
+				}
+			}
+		}
+		else {
+			InterlockedExchangePointer((PVOID*)(&(m_ptrWrite)), next);  // 注意用 next 赋值
+		}
+	}
+}
+
+
+
+
+// =============================================================================================================
+// class Logger
+
+Logger::Logger(const std::string& _loggername, LogLevel _level, std::shared_ptr<LogAppender> _appender)
+{
+	// 注意 getDefaultLogEventManager() 这段代码保证了 LogEventManager 在 Logger 之前创建，确保了 LogEventManager 最后才销毁
+	if (getDefaultLogEventManager()->isFindLogger(_loggername) == true) {
+		throw std::logic_error("Logger::Logger(): loggername must be unqiue, <_loggername> is already exists");
+	}
+
+	m_level = _level;
+	m_loggername = _loggername;
+	getDefaultLogEventManager()->addAppender(_loggername, _appender);
+
+	return;
+}
+
+
+void Logger::log(LogLevel _level, const char* _filename, int _line, const char* _format, ...)
+{
+	if ((int)m_level > (int)_level) {
+		return;
+	}
+
+	va_list args;
+	va_start(args, _format);
+	int size = vsnprintf(nullptr, 0, _format, args);
+	char* buffer = (char*)malloc(sizeof(char) * (size + 1));    // +1保存结尾的"\0"
+	vsprintf(buffer, _format, args);
+	va_end(args);
+
+	LogEvent data;
+	data.m_timestamp = time(nullptr);
+	data.m_loggername = new std::string(m_loggername);
+	data.m_LogLevel = _level;
+	data.m_filename = _filename;
+	data.m_line = _line;
+	data.m_content = buffer;
+
+	getDefaultLogEventManager()->addLogEvent(data);
+}
+
+std::string Logger::getLoggerName()
+{
+	return m_loggername;
+}
+
+bool Logger::addAppender(std::shared_ptr<LogAppender> _appender)
+{
+	getDefaultLogEventManager()->addAppender(m_loggername, _appender);
+	return true;
+}
+
+LogLevel Logger::getLogLevel()
+{
+	return m_level;
+}
+
+void Logger::setLogLevel(LogLevel _level)
+{
+	m_level = _level;
+}
+
+
+
+
+
+
+
---
+++ LearningPlatform/server/src/log/log.h
@@ -0,0 +1,293 @@
+/*// 介绍与说明
+* 1.该日志系统用的是：生产者-消费者多线程模型
+* 
+*		                 +--------------+
+*		线程1 ---------->|              |   LogEventManager异步线程    
+*		线程2 ---------->|  锁(限制写入) | -------------------------->
+*		线程n ---------->|              |
+*		                 +--------------+
+* 
+*	注意：由于LogEventManager是异步运行的，不会影响主线程，所以输出的日志有滞后性，且由于线程间的抢占，可能出现输出的日志信息是乱序的情况。
+* 
+* 2.LogEventManager中三个LogEventQueue指针实现无锁结构处理数据
+*	(1).LogEventQueue是用自定义的双向链表实现的。
+*   (2).read,write,delete 三个指针对应三种操作，这三种操作按先后顺序依次进行，先write后read再delete，
+*	    且 read 可以等于 write, delete = read->prev，彼此之间不会冲突。
+*   
+*		 delete             read               write
+*		   |                 |                   |
+*		+--+--+       +------+-----+      +------+-----+ 
+*		| ... | <---->| LogEvent01 |<---->| LogEvent02 | ----> nullptr 
+*		+-----+       +------------+      +------------+ 
+* 
+* 3.注意Logger的Loggername是具有唯一性的，程序会维护该特性。
+* 4.在使用该日志库时，不建议直接创建全局变量，而是将其封装在一个全局的函数中，函数返回static的Logger，这样更安全，更易于共享该Logger。
+* 5.要配合配置系统保存当前使用的文件名，才能实现续写功能，不然一旦文件满了，每次启动程序都会发生文件滚动。
+* 6.为了多线程下的安全性和输出的日志的稳定性，目前不支持根据loggername查找或删除logaddender
+* 7.Logger::setUpSyncDebug 调用该函数后，只会由最先调用该函数的Logger进行同步的日志输出，其他Logger的输出会被忽略，建议写在Logger初始的构建函数中。
+* 8.日志输出格式：[年-月-日 时:分:秒][日志器名称][日志等级][文件名]:[行号][日志信息]
+* 9.文件滚动的命名格式：文件名_log_0000-00-00#00-00-00.txt
+* 10.日志信息的描述格式：完整的函数名():错误类型, 对该问题的描述可以<变量名>更详细的描述该问题
+*	常用的错误类型如下:
+*		index out of range		下标越界
+*		value out of range      数值越界
+*		valid key               无效键值
+*		null pointer            指针为空 
+*		null [data]             数据为空
+*		open [file] failed      打开文件失败
+*		[data] type error       数据类型错误
+*	    [data] must be unique   数据必须唯一
+*
+*/
+
+
+
+
+/*// 还未解决的问题
+* 1. 
+* 
+* 
+*/
+
+
+/*// 所学到的知识
+* 1.
+* 
+*/
+
+
+
+#pragma once
+#ifndef _MOCHEN_LOG_H_
+#define _MOCHEN_LOG_H_
+
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdexcept>  // throw std::logic_error 
+#include <memory>     // std::share_ptr
+#include <thread>
+#include <mutex>
+#include <string>
+#include <sstream>
+#include <list>
+#include <map>
+
+#include <Windows.h>
+#include <stdarg.h>
+#include <time.h>
+
+
+
+namespace mochen
+{
+
+namespace log
+{
+
+// 日志级别
+enum class LogLevel
+{
+	debug = 0,
+	info,
+	warn,
+	error,
+	fatal
+};
+
+
+// 日志事件
+struct LogEvent
+{
+	time_t	     m_timestamp;
+	std::string *m_loggername; // 申请内存了需要手动释放 
+	LogLevel     m_LogLevel;
+	const char  *m_filename;
+	int		     m_line;
+	char        *m_content;    // 申请内存了需要手动释放 
+};
+
+
+// 格式化函数
+typedef void (*PLogFormatFuntion)(std::stringstream& _ss, LogEvent& _logEvent) ;
+
+// [年-月-日 时:分:秒][日志器名称][日志等级][文件名]:[行号][日志信息]
+extern void logFormatFuntion_detailed(std::stringstream& _ss, LogEvent& _logEvent);
+
+// [时:分:秒][日志等级][简要的文件名]:[行号][日志信息]
+extern void logFormatFuntion_briefly(std::stringstream& _ss, LogEvent& _logEvent);
+
+
+// 日志输出目的地
+class LogAppender
+{
+protected:
+	PLogFormatFuntion m_pLogFormatFuntion;
+public:
+	LogAppender();
+	virtual ~LogAppender() {}                    // 虚析构函数需要实现函数的定义，如果只写函数说明，可能出现链接错误
+
+	virtual void log(const char* _massage) = 0;  // 纯虚函数
+	PLogFormatFuntion getFormatFuntion();
+	void setFormatFuntion(PLogFormatFuntion _pLogFormatFuntion);
+};
+
+
+class ConsoleLogAppender : public LogAppender
+{
+public:
+	ConsoleLogAppender();
+	ConsoleLogAppender(PLogFormatFuntion _pLogFormatFuntion);
+	void log(const char* _massage) override;
+};
+
+
+class FileLogAppender : public LogAppender
+{
+private:
+	std::string m_path;
+	std::string m_filename;
+	FILE* m_fp;
+	int m_maxSize;
+#define M_FILEMAXSIZE 1048576    // 10MB
+public:
+	FileLogAppender();
+	FileLogAppender(const std::string& _filename, int _maxSize = M_FILEMAXSIZE);
+	~FileLogAppender();
+
+	FileLogAppender(const FileLogAppender& _value) = delete;  // 不能拷贝，因为实现滚动机制时要改变文件名。
+	FileLogAppender(FileLogAppender&& _value) noexcept ;
+
+	void operator=(const FileLogAppender& _value) = delete;
+	void operator=(FileLogAppender&& _value) noexcept;
+
+	void log(const char* _massage) override;
+	void clear();
+	void open(const std::string& _filename);
+	// std::string setFilename();      // 为了m_filename和m_fp相匹配，禁止用户直接改文件名，而是通过open去改文件名
+	std::string getFilename();
+
+	int getFileSize();
+	void setFileSize(int _size);
+	void scrolling();   // 日志的滚动机制，要配合配置系统保存当前使用的文件名，才能实现续写功能！！！
+};
+
+
+// 日志事件管理器
+class LogEventManager
+{
+private:
+	struct LogEventQueue
+	{
+		LogEvent m_data;
+		LogEventQueue* m_next;
+	};
+private:
+	std::thread    m_thread;
+	std::mutex     m_mutex;
+	LogEventQueue* m_logEventQueue;
+	LogEventQueue* m_ptrWrite;   // 原子操作的对象
+	LogEventQueue* m_ptrRead;    // 原子操作的对象
+	bool m_isCanExit;
+	std::map<std::string, std::list<std::shared_ptr<LogAppender>>>* m_LogAppenderListMap; // LogAppender是虚基类，赋值时无法调用派生类的拷贝或移动函数，因此LogAppender*。同时因为满足第三种内存管理情况，为了方便管理内存，能智能指针。
+public:
+	LogEventManager();
+	~LogEventManager();
+
+	LogEventManager(const LogEventManager& _value) = delete;
+	LogEventManager(LogEventManager&& _value) noexcept = delete;
+
+	LogEventManager& operator=(const LogEventManager& _value) = delete;
+	LogEventManager& operator=(LogEventManager&& _value) noexcept = delete;
+
+	void clearLogEventNodeData(LogEventQueue* _node);
+	void clearLogEventQueue();
+
+	void clearLogAppenderListMap();
+	void addAppender(std::string _loggername, std::shared_ptr<LogAppender> _appender);   // 注意该数由Logger::addAppender调用，空间是独立的，因此不会线程安全问题
+	bool isFindLogger(const std::string& _loggername);
+	std::map<std::string, std::list<std::shared_ptr<LogAppender>>>* getLogAppenderListMap();
+
+	void dealLogEvent_threadFuntion();
+	void addLogEvent(LogEvent _logEvent);   // 该函数需要线程安全
+};
+
+// 声明全局函数，代替在头文件中声明全局变量，且返回的指针类型，避免调用拷贝或移动相关函数
+extern LogEventManager* getDefaultLogEventManager();
+
+
+// 日志器
+class Logger
+{
+private:
+	std::string m_loggername; 
+	LogLevel m_level;
+public:
+	Logger(const std::string& _loggername, LogLevel _level = LogLevel::debug, std::shared_ptr<LogAppender> _appender = std::make_shared<ConsoleLogAppender>());   // 注意：虽然DefaultLogAppender先被创建，但它的资源释放由LogEventManager控制
+	~Logger() = default;   // LogAppender的释放交给LogEventManager
+
+	Logger(const Logger& _logger) = delete;
+	Logger(Logger&& _logger) noexcept = delete;
+
+	Logger& operator=(const Logger& _logger) = delete;
+	Logger& operator=(Logger&& _logger) noexcept = delete;
+
+	void log(LogLevel _level,const char* _filename, int _line, const char* _format, ...);
+
+	// void setLoggerName(const std::string _loggerName);    // 禁止用户改 loggerName
+	std::string getLoggerName();
+
+	bool addAppender(std::shared_ptr<LogAppender> _appender);       // 注意建议在创建线程之前用该函数，以免造成冲突的情况
+	// bool deleteAppender(....);                                   // 根据实际考量，暂时不实现deleteAppender功能
+
+	LogLevel getLogLevel();
+	void setLogLevel(LogLevel _level);
+};
+
+// 声明全局函数，代替在头文件中声明全局变量，且返回的指针类型，避免调用拷贝或移动相关函数
+extern Logger* getDefaultLogger();
+
+
+// 注意因为宏函数要在其他文件中展开，所以函数中所有用到的函数或者变量，都要加上完整的命名空间名
+#define default_debug(_format, ...) \
+	mochen::log::getDefaultLogger()->log(mochen::log::LogLevel::debug, __FILE__, __LINE__, _format, ##__VA_ARGS__)
+
+#define default_info(_format, ...) \
+	mochen::log::getDefaultLogger()->log(mochen::log::LogLevel::info, __FILE__, __LINE__, _format, ##__VA_ARGS__)
+
+#define default_warn(_format, ...) \
+	mochen::log::getDefaultLogger()->log(mochen::log::LogLevel::warn, __FILE__, __LINE__, _format, ##__VA_ARGS__)
+
+#define default_error(_format, ...) \
+	mochen::log::getDefaultLogger()->log(mochen::log::LogLevel::error, __FILE__, __LINE__, _format, ##__VA_ARGS__)
+
+#define default_fatal(_format, ...) \
+	mochen::log::getDefaultLogger()->log(mochen::log::LogLevel::fatal, __FILE__, __LINE__, _format, ##__VA_ARGS__)
+
+
+#define logger_debug(_logger, _format, ...) \
+	(_logger)->log(mochen::log::LogLevel::debug, __FILE__, __LINE__, _format, ##__VA_ARGS__)
+
+#define logger_info(_logger, _format, ...) \
+	(_logger)->log(mochen::log::LogLevel::info, __FILE__, __LINE__, _format, ##__VA_ARGS__)
+
+#define logger_warn(_logger, _format, ...) \
+	(_logger)->log(mochen::log::LogLevel::warn, __FILE__, __LINE__, _format, ##__VA_ARGS__)
+
+#define logger_error(_logger, _format, ...) \
+	(_logger)->log(mochen::log::LogLevel::error, __FILE__, __LINE__, _format, ##__VA_ARGS__)
+
+#define logger_fatal(_logger, _format, ...) \
+	(_logger)->log(mochen::log::LogLevel::fatal, __FILE__, __LINE__, _format, ##__VA_ARGS__)
+
+
+
+};
+
+};
+
+
+
+#endif // !_MOCHEN_LOG_H_
+
+
